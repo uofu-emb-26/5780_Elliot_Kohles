0982e60e3f9a0908234fd84dc3cf468600fb05fe
ELLIOT KOHLES <u1179751@lab1-27.eng.utah.edu>
2026-03-31 16:56:41 -0600

    Initialize lab7. Added code from the lab and from the Canvas announcement.

--- lab7/Src/lab7.c
+++ lab7/Src/lab7.c
@@ -2,21 +2,46 @@
 #include <stdlib.h>
 
 #include "main.h"
+#include "stm32f0xx.h"
 #include "stm32f0xx_hal.h"
+#include "motor.h"
+#include "SEGGER_RTT.h"
+
+/* -------------------------------------------------------------------------------------------------------------
+ *  Global Variable Declarations
+ *  -------------------------------------------------------------------------------------------------------------
+ */
+volatile uint32_t debouncer;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 
-/**
- * @brief  The application entry point.
- * @retval int
+/* -------------------------------------------------------------------------------------------------------------
+ * Main Program Code
+ *
+ * Starts initialization of peripherals
+ * Blinks green LED (PC9) in loop as heartbeat
+ * -------------------------------------------------------------------------------------------------------------
  */
+volatile uint32_t encoder_count = 0;
+
 int main(void) {
   HAL_Init();
   SystemClock_Config();
   MX_GPIO_Init();
 
+  debouncer = 0;                          // Initialize global variables
+  HAL_Init();                             // Initialize HAL internals
+  LED_init();                             // Initialize LED's
+  button_init();                          // Initialize button
+  motor_init();                           // Initialize motor code
+
   while (1) {
+    GPIOC->ODR ^= GPIO_ODR_9;           // Toggle green LED (heartbeat)
+    encoder_count = TIM2->CNT;
+    HAL_Delay(128);                      // Delay 1/8 second
+
+    // Motor test code
     // Forward
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);   // ENABLE = 1
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);   // INPUT1 = 1
@@ -36,7 +61,69 @@ int main(void) {
     // Stop
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); // ENABLE = 0
     HAL_Delay(1000);
+
+    // Motor test code
   }
+}
+
+/* -------------------------------------------------------------------------------------------------------------
+ *  Miscellaneous Core Functions
+ *  -------------------------------------------------------------------------------------------------------------
+ */
+
+void LED_init(void) {
+    // Initialize PC8 and PC9 for LED's
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;                                          // Enable peripheral clock to GPIOC
+    GPIOC->MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0;                  // Set PC8 & PC9 to outputs
+    GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9);                    // Set to push-pull output type
+    GPIOC->OSPEEDR &= ~((GPIO_OSPEEDR_OSPEEDR8_0 | GPIO_OSPEEDR_OSPEEDR8_1) |
+                        (GPIO_OSPEEDR_OSPEEDR9_0 | GPIO_OSPEEDR_OSPEEDR9_1));   // Set to low speed
+    GPIOC->PUPDR &= ~((GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR8_1) |
+                      (GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR9_1));             // Set to no pull-up/down
+    GPIOC->ODR &= ~(GPIO_ODR_8 | GPIO_ODR_9);                                   // Shut off LED's
+}
+
+void  button_init(void) {
+    // Initialize PA0 for button input
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;                                          // Enable peripheral clock to GPIOA
+    GPIOA->MODER &= ~(GPIO_MODER_MODER0_0 | GPIO_MODER_MODER0_1);               // Set PA0 to input
+    GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR0_0 | GPIO_OSPEEDR_OSPEEDR0_1);     // Set to low speed
+    GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_1;                                        // Set to pull-down
+}
+
+/* Called by SysTick Interrupt
+ * Performs button debouncing, changes wave type on button rising edge
+ * Updates frequency output from ADC value
+ */
+void Lab7_Systick_Callback(void) {
+    // Remember that this function is called by the SysTick interrupt
+    // You can't call any functions in here that use delay
+
+    debouncer = (debouncer << 1);
+    if(GPIOA->IDR & (1 << 0)) {
+        debouncer |= 0x1;
+    }
+
+    if(debouncer == 0x7FFFFFFF) {
+        // Begin critical section
+        __disable_irq();
+        switch(target_rpm) {
+            case 80:
+                target_rpm = 50;
+                break;
+            case 50:
+                target_rpm = 81;
+                break;
+            case 0:
+                target_rpm = 80;
+                break;
+            default:
+                target_rpm = 0;
+                break;
+        }
+        __enable_irq();
+        // End critical section
+    }
 }
 
 static void MX_GPIO_Init(void) {
